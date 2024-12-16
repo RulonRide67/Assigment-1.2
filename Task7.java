e9afe1f4d82ef052117e96e9066cb0bf7ed59916
RulonRide67 <armismadiar20@gmail.com>
2024-12-16 17:32:35 +0500

    Create Task7.java

---
+++ Task7.java
@@ -0,0 +1,14 @@
+public class Task7 {
+    public static void main(String[] args) {
+    int a = 0, b = 0;
+    while (a < 5) {
+        b = 0;
+        System.out.println();
+        while (b < 10) {
+            System.out.print("Q");
+            b++;
+        }
+        a++;
+    }
+  }
+}
