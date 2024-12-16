78ad6a513205089171bec230b2c80d7109b2e73a
RulonRide67 <armismadiar20@gmail.com>
2024-12-16 17:28:53 +0500

    Create Task5.java

---
+++ Task5.java
@@ -0,0 +1,10 @@
+public class Task5 {
+    public static void main(String[] args) {
+        int n = 0;
+        String str = "I will never work for pennies";
+        while (n < 100) {
+            System.out.println(str);
+            n++;
+        }
+    }
+}
