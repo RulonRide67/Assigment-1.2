661d9c4e68c446ce147782cae71ed0eb525932d5
RulonRide67 <armismadiar20@gmail.com>
2024-12-16 17:51:14 +0500

    Create Task11.java

---
+++ Task11.java
@@ -0,0 +1,10 @@
+public class Task11 {
+    public static void main(String[] args) {
+        for (int i = 10; i >=0; i--) {
+            System.out.println();
+            for (int j = i; j < 10; j++) {
+                System.out.print(8);
+            }
+        }
+    }
+}
