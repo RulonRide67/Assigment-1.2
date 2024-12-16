0301ac3ce19c0caeb1ce86b8815d3ed1a5525987
RulonRide67 <armismadiar20@gmail.com>
2024-12-16 17:49:13 +0500

    Create Task10.java

---
+++ Task10.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+public class Task10 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        int c = sc.nextInt();
+        int sum = 0;
+        for (int i = a; i < b; i++) {
+            if (i % c != 0) {
+              sum += i;
+            }
+        }
+        System.out.println(sum);
+    }
+}
